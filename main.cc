04700e37148ee734dbe2b3eda75604562ea718eb
Matt Maly <matthew.r.maly@gmail.com>
2021-06-05 13:54:45 -0700

    Update simple example main() to use more appropriate GMP functions.

--- main.cc
+++ main.cc
@@ -1,20 +1,18 @@
 #include <gmp.h>
 #include <iostream>
+#include <memory>
 
 int main(int argc, char** argv) {
     // Create a GMP integer.
     mpz_t value;
-    mpz_init(value);
-    mpz_set_ui(value, 43);
+    mpz_init_set_ui(value, 43);
 
     // Text-format the GMP integer to a char* buffer and print its contents to
     // stdout.
-    char* text_value;
-    gmp_asprintf(&text_value, "%Zd", value);
-    std::cout << "Test value: " << text_value << std::endl;
+    std::unique_ptr<char[]> text_value(mpz_get_str(/*str=*/nullptr, /*base=*/10, value));
+    std::cout << "Test value: " << text_value.get() << std::endl;
 
     // Free all used memory.
-    delete text_value;
     mpz_clear(value);
 
     return 0;
