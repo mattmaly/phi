508e709a2ab1450960e435291e23a5e9d898c601
Matt Maly <matthew.r.maly@gmail.com>
2021-06-06 08:03:12 -0700

    Add unit tests to cover BigInt's constructor and ToString() method

--- testing/phi-tests.cc
+++ testing/phi-tests.cc
@@ -1,8 +1,14 @@
 #define CATCH_CONFIG_MAIN
-#include "external-deps/catch2/catch.hpp"
 
 #include <string>
 
-TEST_CASE("Empty test") {
-    REQUIRE(1 + 2 == 3);
+#include "external-deps/catch2/catch.hpp"
+#include "phi/bigint.h"
+
+TEST_CASE("Construct a BigInt from an unsigned long") {
+    BigInt small_value(43);
+    REQUIRE(small_value.ToString() == "43");
+
+    BigInt large_value(4294967295);
+    REQUIRE(large_value.ToString() == "4294967295");
 }
