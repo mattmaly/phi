1d5631a56efe7946d70479aaa4eeca1e0cebfc8d
Matt Maly <matthew.r.maly@gmail.com>
2021-06-06 07:42:24 -0700

    Add comments to BigInt header & implementaiton.

--- src/phi/bigint.cc
+++ src/phi/bigint.cc
@@ -11,6 +11,12 @@ BigInt::~BigInt() {
 }
 
 std::string BigInt::ToString() const {
+    // When passed a nullptr first argument, `mpz_get_str()` dynamically
+    // allocates a char[] containing the text-formatted representation of
+    // value_.
+    //
+    // We immediately wrap the char[] in a std::unique_ptr to prevent leaks,
+    // and then we copy its contents into a std::string.
     std::unique_ptr<char[]> text_value(
         mpz_get_str(/*str=*/nullptr, /*base=*/10, value_));
     return std::string(text_value.get());
--- src/phi/bigint.h
+++ src/phi/bigint.h
@@ -6,9 +6,14 @@
 
 class BigInt {
 public:
+    // Initializes a BigInt with the given `initial_value`.
     explicit BigInt(unsigned long initial_value);
+
+    // Frees all memory associated with this BigInt.
     ~BigInt();
 
+    // Returns a std::string containing the base-10 representation of this
+    // BigInt.
     std::string ToString() const;
 private:
     mpz_t value_;
